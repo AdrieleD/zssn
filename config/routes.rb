2526820812b35dc85e43a7184621b8a6c66d083f
AdrieleD <adriele.dutra.souza@hotmail.com>
2018-01-24 02:06:36 -0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :contamination_reports
+  root 'welcome#index'
   resources :inventories
   resources :survivors
+
+  get 'welcome' => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
